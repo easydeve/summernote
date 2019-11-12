7d457a9e889605d076112ed4ddc00247a85e9871
easydeve <easydeve@outlook.com>
2019-11-12 22:28:13 +0700

    Update summernote-th-TH.js
    please upldate lang

--- lang/summernote-th-TH.js
+++ lang/summernote-th-TH.js
@@ -11,7 +11,7 @@
         strikethrough: 'ขีดฆ่า',
         subscript: 'ตัวห้อย',
         superscript: 'ตัวยก',
-        size: 'ขนาดตัวอักษร',
+        size: 'ขนาดตัวอักษร'
       },
       image: {
         image: 'รูปภาพ',
@@ -22,25 +22,25 @@
         floatLeft: 'ชิดซ้าย',
         floatRight: 'ชิดขวา',
         floatNone: 'ไม่จัดตำแหน่ง',
-        shapeRounded: 'Shape: Rounded',
-        shapeCircle: 'Shape: Circle',
-        shapeThumbnail: 'Shape: Thumbnail',
-        shapeNone: 'Shape: None',
+        shapeRounded: 'รูปร่าง: โค้งมน',
+        shapeCircle: 'รูปร่าง: วงกลม',
+        shapeThumbnail: 'รูปร่าง: ภาพตัวอย่าง',
+        shapeNone: 'รูปร่าง: ไม่มี',
         dragImageHere: 'ลากรูปภาพที่ต้องการไว้ที่นี่',
         dropImage: 'วางรูปภาพหรือข้อความ',
         selectFromFiles: 'เลือกไฟล์รูปภาพ',
-        maximumFileSize: 'ขนาดไฟล์ใหญ่สุด',
-        maximumFileSizeError: 'ไฟล์เกินขนาดที่กำหนด',
+        maximumFileSize: 'ขนาดไฟล์สูงสุด',
+        maximumFileSizeError: 'เกินขนาดไฟล์สูงสุด',
         url: 'ที่อยู่ URL ของรูปภาพ',
         remove: 'ลบรูปภาพ',
-        original: 'Original',
+        original: 'ต้นฉบับ'
       },
       video: {
         video: 'วีดีโอ',
         videoLink: 'ลิงก์ของวีดีโอ',
         insert: 'แทรกวีดีโอ',
         url: 'ที่อยู่ URL ของวีดีโอ',
-        providers: '(YouTube, Vimeo, Vine, Instagram, DailyMotion หรือ Youku)',
+        providers: '(YouTube, Vimeo, Vine, Instagram, DailyMotion หรือ Youku)'
       },
       link: {
         link: 'ตัวเชื่อมโยง',
@@ -49,7 +49,7 @@
         edit: 'แก้ไข',
         textToDisplay: 'ข้อความที่ให้แสดง',
         url: 'ที่อยู่เว็บไซต์ที่ต้องการให้เชื่อมโยงไปถึง?',
-        openInNewWindow: 'เปิดในหน้าต่างใหม่',
+        openInNewWindow: 'เปิดในหน้าต่างใหม่'
       },
       table: {
         table: 'ตาราง',
@@ -59,10 +59,10 @@
         addColRight: 'เพิ่มคอลัมน์ด้านขวา',
         delRow: 'ลบแถว',
         delCol: 'ลบคอลัมน์',
-        delTable: 'ลบตาราง',
+        delTable: 'ลบตาราง'
       },
       hr: {
-        insert: 'แทรกเส้นคั่น',
+        insert: 'แทรกเส้นคั่น'
       },
       style: {
         style: 'รูปแบบ',
@@ -74,16 +74,16 @@
         h3: 'หัวข้อ 3',
         h4: 'หัวข้อ 4',
         h5: 'หัวข้อ 5',
-        h6: 'หัวข้อ 6',
+        h6: 'หัวข้อ 6'
       },
       lists: {
         unordered: 'รายการแบบไม่มีลำดับ',
-        ordered: 'รายการแบบมีลำดับ',
+        ordered: 'รายการแบบมีลำดับ'
       },
       options: {
         help: 'ช่วยเหลือ',
         fullscreen: 'ขยายเต็มหน้าจอ',
-        codeview: 'ซอร์สโค้ด',
+        codeview: 'ซอร์สโค้ด'
       },
       paragraph: {
         paragraph: 'ย่อหน้า',
@@ -92,7 +92,7 @@
         left: 'จัดหน้าชิดซ้าย',
         center: 'จัดหน้ากึ่งกลาง',
         right: 'จัดหน้าชิดขวา',
-        justify: 'จัดบรรทัดเสมอกัน',
+        justify: 'จัดบรรทัดเสมอกัน'
       },
       color: {
         recent: 'สีที่ใช้ล่าสุด',
@@ -102,7 +102,7 @@
         transparent: 'โปร่งแสง',
         setTransparent: 'ตั้งค่าความโปร่งแสง',
         reset: 'คืนค่า',
-        resetToDefault: 'คืนค่ามาตรฐาน',
+        resetToDefault: 'คืนค่ามาตรฐาน'
       },
       shortcut: {
         shortcuts: 'แป้นลัด',
@@ -111,45 +111,45 @@
         action: 'การกระทำ',
         paragraphFormatting: 'การจัดรูปแบบย่อหน้า',
         documentStyle: 'รูปแบบของเอกสาร',
-        extraKeys: 'Extra keys',
+        extraKeys: 'ปุ่มเสริม'
       },
       help: {
-        'insertParagraph': 'Insert Paragraph',
-        'undo': 'Undoes the last command',
-        'redo': 'Redoes the last command',
-        'tab': 'Tab',
-        'untab': 'Untab',
-        'bold': 'ทำตัวหนา',
-        'italic': 'ทำตัวเอียง',
-        'underline': 'Set a underline style',
-        'strikethrough': 'Set a strikethrough style',
-        'removeFormat': 'Clean a style',
-        'justifyLeft': 'Set left align',
-        'justifyCenter': 'Set center align',
-        'justifyRight': 'Set right align',
-        'justifyFull': 'Set full align',
-        'insertUnorderedList': 'Toggle unordered list',
-        'insertOrderedList': 'Toggle ordered list',
-        'outdent': 'Outdent on current paragraph',
-        'indent': 'Indent on current paragraph',
-        'formatPara': 'Change current block\'s format as a paragraph(P tag)',
-        'formatH1': 'เปลี่ยนรูปแบบบล็อคปัจจุบันเป็น H1',
-        'formatH2': 'เปลี่ยนรูปแบบบล็อคปัจจุบันเป็น H2',
-        'formatH3': 'เปลี่ยนรูปแบบบล็อคปัจจุบันเป็น H3',
-        'formatH4': 'เปลี่ยนรูปแบบบล็อคปัจจุบันเป็น H4',
-        'formatH5': 'เปลี่ยนรูปแบบบล็อคปัจจุบันเป็น H5',
-        'formatH6': 'เปลี่ยนรูปแบบบล็อคปัจจุบันเป็น H6',
-        'insertHorizontalRule': 'Insert horizontal rule',
-        'linkDialog.show': 'เปิดหน้าแก้ไข Link',
+        'insertParagraph': 'แทรกย่อหน้า',
+        'undo': 'ยกเลิกคำสั่งล่าสุด',
+        'redo': 'ทำซ้ำคำสั่งล่าสุด',
+        'tab': 'แท็บ',
+        'untab': 'ยกเลิกแท็บ',
+        'bold': 'กำหนดรูปแบบตัวหนา',
+        'italic': 'กำหนดรูปแบบตัวเอียง',
+        'underline': 'กำหนดรูปแบบขีดเส้นใต้',
+        'strikethrough': 'กำหนดรูปแบบขีดทับ',
+        'removeFormat': 'ล้างการกำหนดรูปแบบ',
+        'justifyLeft': 'ตั้งค่าจัดชิดซ้าย',
+        'justifyCenter': 'ตั้งค่าจัดกึ่งกลาง',
+        'justifyRight': 'ตั้งค่าจัดชิดขวา',
+        'justifyFull': 'ตั้งค่าจัดเต็มแนว',
+        'insertUnorderedList': 'แทรกหรือลบรายการแบบไม่มีลำดับ',
+        'insertOrderedList': 'แทรกหรือลบรายการแบบมีลำดับ',
+        'outdent': 'เยื้องซ้ายย่อหน้า',
+        'indent': 'เยื้องขวาย่อหน้า',
+        'formatPara': 'เปลี่ยนรูปแบบบล็อกปัจจุบันเป็นย่อหน้า (แท็ก P)',
+        'formatH1': 'เปลี่ยนรูปแบบบล็อกปัจจุบันเป็น H1',
+        'formatH2': 'เปลี่ยนรูปแบบบล็อกปัจจุบันเป็น H2',
+        'formatH3': 'เปลี่ยนรูปแบบบล็อกปัจจุบันเป็น H3',
+        'formatH4': 'เปลี่ยนรูปแบบบล็อกปัจจุบันเป็น H4',
+        'formatH5': 'เปลี่ยนรูปแบบบล็อกปัจจุบันเป็น H5',
+        'formatH6': 'เปลี่ยนรูปแบบบล็อกปัจจุบันเป็น H6',
+        'insertHorizontalRule': 'แทรกเส้นแนวนอน',
+        'linkDialog.show': 'แสดงกล่องข้อความตัวเชื่อมโยง'
       },
       history: {
         undo: 'ยกเลิกการกระทำ',
-        redo: 'ทำซ้ำการกระทำ',
+        redo: 'ทำซ้ำการกระทำ'
       },
       specialChar: {
-        specialChar: 'SPECIAL CHARACTERS',
-        select: 'Select Special characters',
-      },
-    },
+        specialChar: 'อักขระพิเศษ',
+        select: 'เลือกอักขระพิเศษ'
+      }
+    }
   });
 })(jQuery);
